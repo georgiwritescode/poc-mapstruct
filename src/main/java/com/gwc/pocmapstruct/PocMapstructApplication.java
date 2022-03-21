6eda4ec4199e27b3a06de692ad865270ae6618dc
Georgi Ivanov <g.ivanov@blubito.com>
2022-03-21 20:16:11 +0200

    Initial commit

---
+++ src/main/java/com/gwc/pocmapstruct/PocMapstructApplication.java
@@ -0,0 +1,13 @@
+package com.gwc.pocmapstruct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocMapstructApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PocMapstructApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gwc/pocmapstruct/PocMapstructApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gwc.pocmapstruct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocMapstructApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
